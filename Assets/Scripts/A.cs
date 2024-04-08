b14cc601ce22fddc3e1069d86d9a6c2346cd0013
17alma41 <17alma41@gmail.com>
2024-04-08 13:05:39 +0200

    loops

---
+++ Assets/Scripts/A.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class A : MonoBehaviour
+{
+    [SerializeField] int start;
+    [SerializeField] int lenght;
+    [SerializeField] float timeBetweenPrints;
+    [SerializeField] GameObject prefab;
+    int numberInstantiated;
+    Vector3 position;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*
+        for (int i = 0; i < times; i++)
+        {
+            //print(i);
+            //print(i+1);
+            //print(-i);
+        }
+        */
+
+        StartCoroutine(loops());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator loops()
+    {
+        for (int i = start; i <= start + lenght -1; i++)
+            for (int j = start; j <= start + lenght - 1; j++)
+            {
+
+                //position = new Vector3(j, i, 0);
+                //InstantiateAt(position);
+                //yield return new WaitForSeconds(timeBetweenPrints);
+
+                //if (i == j) //Diagonal
+                //{
+                //    position = new Vector3(j, i, 0);
+                //    InstantiateAt(position);
+                //    yield return new WaitForSeconds(timeBetweenPrints); 
+                //}
+
+                if (true) //Marco
+                {
+                    position = new Vector3(j, i, 0);
+                    InstantiateAt(position);
+                    yield return new WaitForSeconds(timeBetweenPrints);
+                }
+            }
+    }
+
+    void InstantiateAt(Vector3 position)
+    {
+        GameObject instantiated = Instantiate(
+            prefab,
+            position,
+            Quaternion.identity
+            );
+
+        instantiated.name = "Sphere " + numberInstantiated++;
+        print("Placing " + instantiated.name + " at " + position);
+
+    }
+}
