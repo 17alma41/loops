64c9b47fb74fe46bfa6a79010dbd92f30f1b7b37
17alma41 <17alma41@gmail.com>
2024-04-10 15:43:42 +0200

    update: create and delete

--- Assets/Scripts/A1.cs
+++ Assets/Scripts/A1.cs
@@ -4,86 +4,65 @@ using UnityEngine;
 
 public class A1 : MonoBehaviour
 {
-    [Header("Instanciar esferas")]
-    [SerializeField] int start;
-    [SerializeField] int lenght;
-    [SerializeField] float timeBetweenPrints;
+    [SerializeField] int start = 0;
+    [SerializeField] int lenght = 5;
+    [SerializeField] float timeBetweenPrints = 0.5f;
     [SerializeField] GameObject prefab;
 
     int numberInstantiated;
     Vector3 position;
+    Coroutine coroutine = null;
 
-    [Header("Rotación del cubo")]
     [SerializeField] bool rotate = false;
     [SerializeField] Vector3 rotationSpeed;
+    List<GameObject> instantedObjects = new List<GameObject>();
 
-    [SerializeField] List<GameObject> instantedObjects = new List<GameObject>();
-
-    // Start is called before the first frame update
     void Start()
     {
-        /*
-        for (int i = 0; i < times; i++)
-        {
-            //print(i);
-            //print(i+1);
-            //print(-i);
-        }
-        */
-
-        StartCoroutine(loops());
+        StartCoroutine(MainLoop());
         StartCoroutine(SphereRotation());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator MainLoop()
     {
+        while (true) { 
+
+            coroutine = StartCoroutine(Loops());
+            while (coroutine != null)
+                yield return null;
+
+            coroutine = StartCoroutine(DestroyObjects());
+            while (coroutine != null)
+                yield return null;
+        }
     }
 
-    IEnumerator loops()
+    IEnumerator Loops()
     {
         for (int i = start; i <= start + lenght - 1; i++)
             for (int j = start; j <= start + lenght - 1; j++)
                 for (int k = start; k <= start + lenght - 1; k++)
                 {
-
-                    //position = new Vector3(j, i, k);
-                    //InstantiateAt(position);
-                    //yield return new WaitForSeconds(timeBetweenPrints);
-
-                    //if (i == j) //Diagonal
-                    //{
-                    //    position = new Vector3(j, i, 0);
-                    //    InstantiateAt(position);
-                    //    yield return new WaitForSeconds(timeBetweenPrints); 
-                    //}
-
-                    //if (i == start || i == start + lenght -1 || j == start || j == start + lenght -1) //Marco
-                    //{
-                    //    position = new Vector3(j, i, 0);
-                    //    InstantiateAt(position);
-                    //    yield return new WaitForSeconds(timeBetweenPrints);
-                    //}
-
                     if (j == start && i == start ||
                         i == start && k == start ||
                         k == start && j == start ||
-                        j == start && i == start + lenght -1 ||
-                        i == start && k == start + lenght -1 ||
-                        k == start && j == start + lenght -1 ||
-                        j == start + lenght -1 && i == start ||
-                        i == start + lenght - 1 && k == start||
+                        j == start && i == start + lenght - 1 ||
+                        i == start && k == start + lenght - 1 ||
+                        k == start && j == start + lenght - 1 ||
+                        j == start + lenght - 1 && i == start ||
+                        i == start + lenght - 1 && k == start ||
                         k == start + lenght - 1 && j == start ||
-                        j == start + lenght -1 && i == start + lenght -1||
-                        i == start + lenght -1 && k == start + lenght -1||
-                        k == start + lenght -1 && j == start + lenght -1) //Cubo con solo las aristas
+                        j == start + lenght - 1 && i == start + lenght - 1 ||
+                        i == start + lenght - 1 && k == start + lenght - 1 ||
+                        k == start + lenght - 1 && j == start + lenght - 1)
                     {
-                       position = new Vector3(j, i, k);
-                       InstantiateAt(position);
-                       yield return new WaitForSeconds(timeBetweenPrints);
+                        position = new Vector3(j, i, k);
+                        InstantiateAt(position);
+                        yield return new WaitForSeconds(timeBetweenPrints);
                     }
                 }
-   
+
+        coroutine = null;
     }
 
     void InstantiateAt(Vector3 position)
@@ -110,14 +89,13 @@ public class A1 : MonoBehaviour
         Normalize(position.y, 0, 5),
         Normalize(position.z, 0, 5));
 
-        StartCoroutine(DestroySphere());
     }
 
     IEnumerator SphereRotation()
     {
         while (true)
         {
-            if(rotate == true)
+            if (rotate == true)
             {
                 transform.Rotate(rotationSpeed * Time.deltaTime);
             }
@@ -125,14 +103,15 @@ public class A1 : MonoBehaviour
         }
     }
 
-    IEnumerator DestroySphere()
+    IEnumerator DestroyObjects()
     {
-        for (int i = 0; i < instantedObjects.Count; i++)
+        while (instantedObjects.Count > 0)
         {
-            Destroy(instantedObjects[i]);
-            instantedObjects.Clear();
+            Destroy(instantedObjects[0]);
+            instantedObjects.RemoveAt(0);
+            yield return new WaitForSeconds(timeBetweenPrints);
         }
-        yield return null;
+
+        coroutine = null;
     }
-    
 }
